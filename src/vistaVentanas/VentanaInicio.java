324412fee0872309b26374dd2d122f50ee810309
Manuel Cardoso <manolocarodoso1501@hotmail.com>
2023-05-10 22:16:20 -0500

    Nombres jugadores (Modificacion interzas de incio)

--- src/vistaVentanas/VentanaInicio.java
+++ src/vistaVentanas/VentanaInicio.java
@@ -12,8 +12,9 @@ import javax.swing.*;
 
 public class VentanaInicio extends JFrame implements ActionListener {
 
+    private JTextField txtplayer1, txtplayer2;
     //Imagenes del juego (5)
-    private Decolib im1, im2, im3, im4, im5, im6;
+    private Decolib im1, im2, im3, im4, im5, im6, im7;
     //boton de jugar
     private JButton btninicarJuego;
     //panel
@@ -41,23 +42,23 @@ public class VentanaInicio extends JFrame implements ActionListener {
         add(im1);
 
         im2 = new Decolib("/Imgs/St/tik.png");
-        im2.setBounds((int)118.78, (int)144.28, 222, 120);
+        im2.setBounds((int)108.78, (int)95.28, 222, 120);
         add(im2);
 
         im3 = new Decolib("/Imgs/St/tac.png");
-        im3.setBounds((int) 207.5, (int) 261.5, 223, 117);
+        im3.setBounds((int) 77.5, (int) 271.5, 223, 117);
         add(im3);
         
         im4 = new Decolib("/Imgs/St/toe.png");
-        im4.setBounds((int) 297.5, (int) 390.5, 234, 117);
+        im4.setBounds((int) 165.5, (int) 435.5, 234, 117);
         add(im4);
 
         im5 = new Decolib("/Imgs/St/xo.png");
-        im5.setBounds((int) 672.75, (int) 97.88, 172, 170);
+        im5.setBounds((int) 702.75, (int) 72.88, 172, 170);
         add(im5);
 
         btninicarJuego = new JButton();
-        btninicarJuego.setBounds(563, 283, 283, 153);
+        btninicarJuego.setBounds((int)490.91, (int) 406.78, 214, 116);
         btninicarJuego.setOpaque(true);
         btninicarJuego.setContentAreaFilled(false);
         btninicarJuego.setBorderPainted(false);
@@ -65,22 +66,84 @@ public class VentanaInicio extends JFrame implements ActionListener {
 
         //añadirle imagen al boton
         im6 = new Decolib("/Imgs/St/btplay.png");
-        im6.setBounds(564,283, 283, 153);
+        im6.setBounds((int)490.91, (int)406.78, 214, 116);
         add(im6);
+
+        im7 = new Decolib("/Imgs/St/players.png");
+        im7.setBounds((int)344.13, 270, 517, 117 );
+        add(im7);
+
+        txtplayer1 = new JTextField("Player 1");
+        txtplayer1.setHorizontalAlignment(SwingConstants.CENTER);
+        txtplayer1.setBounds(376,301,204,59);
+        txtplayer1.setForeground(Color.white);
+        txtplayer1.setFont(new Font("arial", Font.ROMAN_BASELINE, 30));
+        txtplayer1.setOpaque(false);
+        txtplayer1.setBorder(BorderFactory.createEmptyBorder());
+        add(txtplayer1);
+
+        txtplayer2 = new JTextField("Player 2");
+        txtplayer2.setHorizontalAlignment(SwingConstants.CENTER);
+        txtplayer2.setBounds(620,301,204,59);
+        txtplayer2.setForeground(Color.white);
+        txtplayer2.setFont(new Font("arial", Font.ROMAN_BASELINE, 30));
+        txtplayer2.setOpaque(false);
+        txtplayer2.setBorder(BorderFactory.createEmptyBorder());
+        add(txtplayer2);
+        
         
         btninicarJuego.addActionListener(this);
 
+
+        txtplayer1.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e) {
+                txtplayer1.setText("");
+            }
+        
+            @Override
+            public void focusLost(FocusEvent e) {
+                //no hacer nada aqui
+            }
+        });
+
+        txtplayer2.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e) {
+                txtplayer2.setText("");
+            }
+        
+            @Override
+            public void focusLost(FocusEvent e) {
+                //no hacer nada aqui
+            }
+        });
+
     }
+
 
     @Override
     public void actionPerformed(ActionEvent evento) {
         // TODO Auto-generated method stub
-       if (evento.getSource() == btninicarJuego); {
-        dispose();
-        VentanaModoJuego ventana = new VentanaModoJuego();
+       if (evento.getSource() == btninicarJuego) {
+        textselec();
        }
     }
 
+    private void textselec(){
+        String nombrep1 = txtplayer1.getText();
+        String nombrep2 = txtplayer2.getText();
+        if(nombrep1.trim().isEmpty() || nombrep1.trim().equals("Player 1") || nombrep2.trim().isEmpty() || nombrep2.trim().equals("Player 2")) {
+            JOptionPane.showMessageDialog(null, "PORFAVOR INGRESE SUS NOMBRES PARA INICAR EN CASO DE JUGAR SOLO INGRESE BOT EN SEGUNDO JUGADOR", "Advertencia", JOptionPane.ERROR_MESSAGE);
+            if(nombrep1.trim().isEmpty() || nombrep1.trim().equals("Player 1")) {
+                txtplayer1.requestFocus();
+            } else {
+                txtplayer2.requestFocus();
+            }
+        } else {
+            dispose();
+            VentanaModoJuego ventana = new VentanaModoJuego();
+        }
+        
 
-
+    }
+    
 }
